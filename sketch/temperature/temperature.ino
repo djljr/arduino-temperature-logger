75e200ff505d273469d9135bed84c8f568d1dd3f
Den Lipovsky <dennis.lipovsky@gmail.com>
2013-02-05 01:04:34 -0500

    adding lcd display to arduino sketch correctly (i think) implement the thermistor reading code for the probe thermometer i have

--- sketch/temperature/temperature.ino
+++ sketch/temperature/temperature.ino
@@ -1,31 +1,89 @@
-#include <OneWire.h>
-#include <DallasTemperature.h>
+#include <LiquidCrystal.h>
 
 // Data wire is plugged into pin 2 on the Arduino
-#define ONE_WIRE_BUS 2
+#define TEMP_1 A0
 
-// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
-OneWire oneWire(ONE_WIRE_BUS);
+LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
 
-// Pass our oneWire reference to Dallas Temperature. 
-DallasTemperature sensors(&oneWire);
+byte degrees_char[8] = {
+  0b01100,
+  0b10010,
+  0b10010,
+  0b01100,
+  0b00000,
+  0b00000,
+  0b00000,
+  0b00000
+};
+
+uint8_t degrees = 0;
 
 void setup(void)
 {
+  lcd.createChar(degrees, degrees_char);
+  lcd.begin(16, 2);
+  lcd.print("Starting Up...");
   // start serial port
   Serial.begin(9600);
-  
+  // connect AREF to 3.3V and use that as VCC, less noisy!
+  analogReference(EXTERNAL);
   // Start up the library
-  sensors.begin(); // IC Default 9 bit. If you have troubles consider upping it 12. Ups the delay giving the IC more time to process the temperature measurement
+  lcd.setCursor(0,1);
+  lcd.print("Started");
+  delay(2500);
 }
 
+// how many samples to take and average, more takes longer
+// but is more 'smooth'
+#define NUMSAMPLES 5
+// the value of the 'other' resistor
+#define SERIESRESISTOR 10000
+// resistance at 25 degrees C
+#define THERMISTORNOMINAL 55000 
+// temp. for nominal resistance (almost always 25 C)
+#define TEMPERATURENOMINAL 25 
+// The beta coefficient of the thermistor (usually 3000-4000)
+#define BCOEFFICIENT 3950
+
+int samples[NUMSAMPLES];
 
 void loop(void)
 { 
-  // call sensors.requestTemperatures() to issue a global temperature 
-  // request to all devices on the bus
-  sensors.requestTemperatures(); // Send the command to get temperatures
-  Serial.println(sensors.getTempFByIndex(0)); // Why "byIndex"? You can have more than one IC on the same bus. 0 refers to the first IC on the wire
-  delay(5000);
+  uint8_t i;
+  float average;
+  // take N samples in a row, with a slight delay
+  for (i=0; i< NUMSAMPLES; i++) {
+    samples[i] = analogRead(TEMP_1);
+    delay(10);
+  }
+  
+  average = 0;
+  for (i=0; i< NUMSAMPLES; i++) {
+    average += samples[i];
+  }
+  average /= NUMSAMPLES;
+  
+  lcd.clear();
+  lcd.print("A0:");
+  lcd.print((int)average);
+
+  lcd.setCursor(0, 1);
+  lcd.print("2:");
+  
+  average = (1023 / average) - 1;
+  average = SERIESRESISTOR / average;
+  
+  float steinhart = average / THERMISTORNOMINAL;
+  steinhart = log(steinhart);
+  steinhart /= BCOEFFICIENT;
+  steinhart += 1.0 / (TEMPERATURENOMINAL + 273.15); 
+  steinhart = 1.0 / steinhart;
+  steinhart -= 273.15;
+  
+  lcd.print(steinhart);
+  lcd.write(degrees);
+  lcd.print("C");
+  
+  delay(1000);
 }
 
